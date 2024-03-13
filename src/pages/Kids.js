fb48111f1d8173d4760beac871d3cf3691611682
chu kin nam <leochu@chudeair.home>
2024-03-12 22:55:03 -0400

    Filter update

--- src/pages/Kids.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Brands = () => {
-  return <div>kids</div>;
-};
-
-export default Brands;
--- src/pages/Man.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Man = () => {
-  return <div>Man</div>;
-};
-
-export default Man;
